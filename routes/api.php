b3ba4e40489a5ba7010eb169ddc2c6fefd5b6922
Abir <abiruzzaman.molla@gmail.com>
2020-02-27 18:20:54 +0600

    Created models, controllers, migrates and routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Buyers api routes
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+// Categories api routes
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+// Products api routes
+Route::resource('productes', 'Product\ProductController', ['only' => ['index', 'show']]);
+// Sellers api routes
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+// Transactions api routes
+Route::resource('transections', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+// User api routes
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
