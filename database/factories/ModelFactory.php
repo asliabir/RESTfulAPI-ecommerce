53b7158daf7e5b8056f2bcc6ff599e9c0376b1ba
Abir <abiruzzaman.molla@gmail.com>
2020-02-29 14:00:09 +0600

    product faker

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Category;
+use App\Product;
 use App\User;
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +34,18 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first()->id,
     ];
 });
